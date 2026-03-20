b13e6f46b8e832f68fa10e379955c29b6c12fe16
Ousmane <ousmanegitgit@gmail.com>
2026-03-20 03:37:31 -0400

    Completed Javascript for Mini Bankist Application

--- banking.js
+++ banking.js
@@ -5,8 +5,30 @@ const account1 = [500, -600, -400, 800]
 const account2 = [-300, -100, 500, 623]
 const account3 = [724, 681, -229, 444]
 
+const accounts = [account1, account2, account3]
+
 // DOM ELEMENTS \\
 
-const movement = document.querySelector('movement-container');
+const movement = document.querySelector('.movement-container');
 
 // RENDER \\
+
+const displayMovements = function(acc) { 
+
+    movement.innerHTML = '';
+
+    acc.forEach((mov, i) => {
+        
+        const type = mov > 0 ? 'deposit' : 'withdraw'
+
+        const html = `<div class="movement">
+             <p class="${type}">${i + 1}:${type}</p>
+             <p class="movement_balance">$${mov}</p>
+            </div>`
+        ;
+        movement.insertAdjacentHTML('beforeend', html)
+        
+    })
+}
+
+displayMovements(accounts[0]);
